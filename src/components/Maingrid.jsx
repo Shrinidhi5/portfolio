39370e176ba03540de1b7e394b08ca45f914cab1
Shrinidhi5 <shripokhrikar@gmail.com>
2026-02-01 15:47:07 +0530

    I added section 1

--- src/components/Maingrid.jsx
+++ src/components/Maingrid.jsx
@@ -2,32 +2,51 @@ import React from 'react'
 import './Maingrid.css'
 const Maingrid = () => {
   return (
-    <div className='gridbox'>
-      <div className='box11'></div>
-      <div className='box12'></div>
-      <div className='box13'></div>
-      <div className='box14'></div>
-      <div className='box15'></div>
-      <div className='box16'></div>
-      <div className='box17'></div>
-      <div className='box18'></div>
-      <div className='box21'></div>
-      <div className='box22'></div>
-      <div className='box23'></div>
-      <div className='box24'></div>
-      <div className='box25'></div>
-      <div className='box26'></div>
-      <div className='box27'></div>
-      <div className='box28'></div>
-      <div className='box31'></div>
-      <div className='box32'></div>
-      <div className='box33'></div>
-      <div className='box34'></div>
-      <div className='box35'></div>
-      <div className='box36'></div>
-      <div className='box37'></div>
-      <div className='box38'></div>
+    <div>
+      <div className='gridbox'>
+        <div className='box11'></div>
+        <div className='box12'></div>
+        <div className='box13'></div>
+        <div className='box14'></div>
+        <div className='box15'></div>
+        <div className='box16'></div>
+        <div className='box17'></div>
+        <div className='box18'></div>
+        <div className='box21'></div>
+        <div className='box22'></div>
+        <div className='box23'></div>
+        <div className='box24'></div>
+        <div className='box25'></div>
+        <div className='box26'></div>
+        <div className='box27'></div>
+        <div className='box28'></div>
+        <div className='box31'></div>
+        <div className='box32'></div>
+        <div className='box33'></div>
+        <div className='box34'></div>
+        <div className='box35'></div>
+        <div className='box36'></div>
+        <div className='box37'></div>
+        <div className='box38'></div>
+      </div>
+      <div className='social_1'>
+        <img src="imgs/blue_cat.png" alt="hiii im here" />
+      </div>
+      <div className='social_2'>
+        <img src="imgs/blue_giboon.png" alt="" />
+      </div>
+      <div className='social_3'>
+        <img src="imgs/blue_mobile.png" alt="" />
+      </div>
+      <div className='social_4'>
+        <img src="imgs/dolly.png" alt="" />
+      </div>
+        <p className='text_1'>
+          Design Creative Instagram
+          Brands with Heart -
+          for Makers, Creators, and Storyteller</p>
     </div>
+
   )
 }
 
